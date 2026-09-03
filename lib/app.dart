3205807cb8488daea24722598682fcb2671d38fe
Merdan <wisew.tube@gmail.com>
2026-09-04 00:43:49 +0500

    Add GoRouter, HomeScreen, and Riverpod setup
    Introduce routing and a placeholder home screen. Added router.dart with a GoRouter configured for '/' and created features/home/home_screen.dart. Wire router into MaterialApp.router (debug banner disabled) in app.dart. main.dart updated to wrap the app with ProviderScope (flutter_riverpod) to prepare for state management.

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:kust/router.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,7 @@
+import 'package:go_router/go_router.dart';
+import 'package:kust/features/home/home_screen.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [GoRoute(path: '/', builder: (context, state) => HomeScreen())],
+);
